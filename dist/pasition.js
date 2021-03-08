76a762d99bc96ca8666ce21bd644bc4139bde45a
Jiang <505514870@qq.com>
2021-03-08 23:11:39 +0800

    feat: curves split

--- dist/pasition.js
+++ dist/pasition.js
@@ -5,9 +5,9 @@
  */
 
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.pasition = factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.pasition = factory());
 }(this, (function () { 'use strict';
 
 //https://github.com/colinmeinke/svg-arc-to-cubic-bezier
@@ -220,157 +220,226 @@ var pasition = {};
 pasition.parser = parse;
 
 pasition.lerpCurve = function (pathA, pathB, t) {
-  return pasition.lerpPoints(pathA[0], pathA[1], pathB[0], pathB[1], t).concat(pasition.lerpPoints(pathA[2], pathA[3], pathB[2], pathB[3], t)).concat(pasition.lerpPoints(pathA[4], pathA[5], pathB[4], pathB[5], t)).concat(pasition.lerpPoints(pathA[6], pathA[7], pathB[6], pathB[7], t));
+    return pasition.lerpPoints(pathA[0], pathA[1], pathB[0], pathB[1], t).concat(pasition.lerpPoints(pathA[2], pathA[3], pathB[2], pathB[3], t)).concat(pasition.lerpPoints(pathA[4], pathA[5], pathB[4], pathB[5], t)).concat(pasition.lerpPoints(pathA[6], pathA[7], pathB[6], pathB[7], t));
 };
 
 pasition.lerpPoints = function (x1, y1, x2, y2, t) {
-  return [x1 + (x2 - x1) * t, y1 + (y2 - y1) * t];
+    return [x1 + (x2 - x1) * t, y1 + (y2 - y1) * t];
 };
 
 pasition.q2b = function (x1, y1, x2, y2, x3, y3) {};
 
 pasition.path2shapes = function (path) {
-  // 解析 svg path
-  var cmds = pasition.parser(path);
-  var preX = 0,
-      preY = 0,
-      j = 0,
-      sLen = void 0,
-      len = cmds.length,
-      closeX = void 0,
-      closeY = void 0,
-      current = void 0,
-      shapes = [];
+    // 解析 svg path
+    var cmds = pasition.parser(path);
+    var preX = 0,
+        preY = 0,
+        j = 0,
+        sLen = void 0,
+        len = cmds.length,
+        closeX = void 0,
+        closeY = void 0,
+        current = void 0,
+        shapes = [];
 
-  for (; j < len; j++) {
-    var item = cmds[j];
-    var action = item[0];
-    var preItem = cmds[j - 1];
+    for (; j < len; j++) {
+        var item = cmds[j];
+        var action = item[0];
+        var preItem = cmds[j - 1];
 
-    switch (action) {
-      case 'M':
+        switch (action) {
+            case 'M':
 
-        sLen = shapes.length;
-        shapes[sLen] = [];
-        current = shapes[sLen];
-        preX = item[1];
-        preY = item[2];
-        break;
+                sLen = shapes.length;
+                shapes[sLen] = [];
+                current = shapes[sLen];
+                preX = item[1];
+                preY = item[2];
+                break;
 
-      case 'L':
+            case 'L':
 
-        current.push([preX, preY, item[1], item[2], item[1], item[2], item[1], item[2]]);
-        preX = item[1];
-        preY = item[2];
+                current.push([preX, preY, item[1], item[2], item[1], item[2], item[1], item[2]]);
+                preX = item[1];
+                preY = item[2];
 
-        break;
+                break;
 
-      case 'Z':
-        closeX = current[0][0];
-        closeY = current[0][1];
-        current.push([preX, preY, closeX, closeY, closeX, closeY, closeX, closeY]);
-        break;
+            case 'Z':
+                closeX = current[0][0];
+                closeY = current[0][1];
+                current.push([preX, preY, closeX, closeY, closeX, closeY, closeX, closeY]);
+                break;
+        }
     }
-  }
 
-  return shapes;
+    return shapes;
 };
 
 pasition._upCurves = function (curves, count) {};
+function split(x1, y1, x2, y2, x3, y3, x4, y4, t) {
+    return {
+        left: _split(x1, y1, x2, y2, x3, y3, x4, y4, t),
+        right: _split(x4, y4, x3, y3, x2, y2, x1, y1, 1 - t, true)
+    };
+}
 
-pasition._splitCurves = function (curves, count) {};
+function _split(x1, y1, x2, y2, x3, y3, x4, y4, t, reverse) {
+
+    var x12 = (x2 - x1) * t + x1;
+    var y12 = (y2 - y1) * t + y1;
+
+    var x23 = (x3 - x2) * t + x2;
+    var y23 = (y3 - y2) * t + y2;
+
+    var x34 = (x4 - x3) * t + x3;
+    var y34 = (y4 - y3) * t + y3;
+
+    var x123 = (x23 - x12) * t + x12;
+    var y123 = (y23 - y12) * t + y12;
+
+    var x234 = (x34 - x23) * t + x23;
+    var y234 = (y34 - y23) * t + y23;
+
+    var x1234 = (x234 - x123) * t + x123;
+    var y1234 = (y234 - y123) * t + y123;
+
+    if (reverse) {
+        return [x1234, y1234, x123, y123, x12, y12, x1, y1];
+    }
+    return [x1, y1, x12, y12, x123, y123, x1234, y1234];
+}
+
+pasition._splitCurves = function (curves, count) {
+    var i = 0,
+        index = 0;
+
+    for (; i < count; i++) {
+        var curve = curves[index];
+        var cs = split(curve[0], curve[1], curve[2], curve[3], curve[4], curve[5], curve[6], curve[7], 0.5);
+        curves.splice(index, 1);
+        curves.splice(index, 0, cs.left, cs.right);
+
+        index += 2;
+        if (index >= curves.length - 1) {
+            index = 0;
+        }
+    }
+};
 
 function sync(shapes, count) {
-  var _loop = function _loop(i) {
-    var shape = shapes[shapes.length - 1];
+    var _loop = function _loop(i) {
+        var shape = shapes[shapes.length - 1];
 
-    var newShape = [];
+        var newShape = [];
 
-    shape.forEach(function (curve) {
-      newShape.push(curve.slice(0));
-    });
+        shape.forEach(function (curve) {
+            newShape.push(curve.slice(0));
+        });
 
-    shapes.push(newShape);
-  };
+        shapes.push(newShape);
+    };
 
-  for (var i = 0; i < count; i++) {
-    _loop(i);
-  }
+    for (var i = 0; i < count; i++) {
+        _loop(i);
+    }
 }
 
 pasition.lerp = function (pathA, pathB, t) {};
 
-pasition.MIM_CURVES_COUNT = 100;
+pasition.MIM_CURVES_COUNT = 10;
 
 pasition._preprocessing = function (pathA, pathB) {
 
-  var lenA = pathA.length,
-      lenB = pathB.length,
-      clonePathA = JSON.parse(JSON.stringify(pathA)),
-      clonePathB = JSON.parse(JSON.stringify(pathB));
+    var lenA = pathA.length,
+        lenB = pathB.length,
+        clonePathA = JSON.parse(JSON.stringify(pathA)),
+        clonePathB = JSON.parse(JSON.stringify(pathB));
 
-  if (lenA > lenB) {
-    sync(clonePathB, lenA - lenB);
-  } else if (lenA < lenB) {
-    sync(clonePathA, lenB - lenA);
-  }
+    if (lenA > lenB) {
+        sync(clonePathB, lenA - lenB);
+    } else if (lenA < lenB) {
+        sync(clonePathA, lenB - lenA);
+    }
 
-  clonePathA = sort(clonePathA, clonePathB);
+    clonePathA = sort(clonePathA, clonePathB);
 
-  clonePathA.forEach(function (curves, index) {
-    clonePathA[index] = sortCurves(curves, clonePathB[index]);
-  });
+    clonePathA.forEach(function (curves, index) {
 
-  return [clonePathA, clonePathB];
+        var lenA = curves.length,
+            lenB = clonePathB[index].length;
+
+        if (lenA > lenB) {
+            if (lenA < pasition.MIM_CURVES_COUNT) {
+                pasition._splitCurves(curves, pasition.MIM_CURVES_COUNT - lenA);
+                pasition._splitCurves(clonePathB[index], pasition.MIM_CURVES_COUNT - lenB);
+            } else {
+                pasition._splitCurves(clonePathB[index], lenA - lenB);
+            }
+        } else if (lenA < lenB) {
+            if (lenB < pasition.MIM_CURVES_COUNT) {
+                pasition._splitCurves(curves, pasition.MIM_CURVES_COUNT - lenA);
+                pasition._splitCurves(clonePathB[index], pasition.MIM_CURVES_COUNT - lenB);
+            } else {
+                pasition._splitCurves(curves, lenB - lenA);
+            }
+        }
+    });
+
+    clonePathA.forEach(function (curves, index) {
+        clonePathA[index] = sortCurves(curves, clonePathB[index]);
+    });
+
+    return [clonePathA, clonePathB];
 };
 
 pasition._lerp = function (pathA, pathB, t) {
-  var shapes = [];
-  pathA.forEach(function (curves, index) {
-    var newCurves = [];
-    curves.forEach(function (curve, curveIndex) {
-      newCurves.push(pasition.lerpCurve(curve, pathB[index][curveIndex], t));
+    var shapes = [];
+    pathA.forEach(function (curves, index) {
+        var newCurves = [];
+        curves.forEach(function (curve, curveIndex) {
+            newCurves.push(pasition.lerpCurve(curve, pathB[index][curveIndex], t));
+        });
+        shapes.push(newCurves);
     });
-    shapes.push(newCurves);
-  });
-  return shapes;
+    return shapes;
 };
 
 pasition.animate = function (option) {
-  var pathA = pasition.path2shapes(option.from);
-  var pathB = pasition.path2shapes(option.to);
+    var pathA = pasition.path2shapes(option.from);
+    var pathB = pasition.path2shapes(option.to);
 
-  var pathArr = pasition._preprocessing(pathA, pathB);
-  console.log('pathArr', pathArr);
+    var pathArr = pasition._preprocessing(pathA, pathB);
+    console.log('pathArr', pathArr);
 
-  var beginTime = new Date(),
-      end = option.end || function () {},
-      progress = option.progress || function () {},
-      begin = option.begin || function () {},
-      easing = option.easing || function (v) {
-    return v;
-  },
-      tickId = null,
-      outShape = null,
-      time = option.time;
+    var beginTime = new Date(),
+        end = option.end || function () {},
+        progress = option.progress || function () {},
+        begin = option.begin || function () {},
+        easing = option.easing || function (v) {
+        return v;
+    },
+        tickId = null,
+        outShape = null,
+        time = option.time;
 
-  begin(pathA);
+    begin(pathA);
 
-  var tick = function tick() {
-    var dt = new Date() - beginTime;
-    if (dt >= time) {
-      outShape = pathB;
-      progress(outShape, 1);
-      end(outShape);
-      cancelAnimationFrame(tickId);
-      return;
-    }
-    var percent = easing(dt / time);
-    outShape = pasition._lerp(pathArr[0], pathArr[1], percent);
-    progress(outShape, percent);
-    tickId = setTimeout(tick, 500);
-  };
-  tick();
+    var tick = function tick() {
+        var dt = new Date() - beginTime;
+        if (dt >= time) {
+            outShape = pathB;
+            progress(outShape, 1);
+            end(outShape);
+            cancelAnimationFrame(tickId);
+            return;
+        }
+        var percent = easing(dt / time);
+        outShape = pasition._lerp(pathArr[0], pathArr[1], percent);
+        progress(outShape, percent);
+        tickId = requestAnimationFrame(tick);
+    };
+    tick();
 };
 
 return pasition;
--- src/my.js
+++ src/my.js
@@ -73,17 +73,55 @@ pasition.path2shapes = function (path) {
 pasition._upCurves = function (curves, count) {
    
 }
-
 function split(x1, y1, x2, y2, x3, y3, x4, y4, t) {
-  
+  return {
+      left: _split(x1, y1, x2, y2, x3, y3, x4, y4, t),
+      right: _split(x4, y4, x3, y3, x2, y2, x1, y1, 1 - t, true)
+  }
 }
 
 function _split(x1, y1, x2, y2, x3, y3, x4, y4, t, reverse) {
 
+
+  let x12 = (x2 - x1) * t + x1
+  let y12 = (y2 - y1) * t + y1
+
+  let x23 = (x3 - x2) * t + x2
+  let y23 = (y3 - y2) * t + y2
+
+  let x34 = (x4 - x3) * t + x3
+  let y34 = (y4 - y3) * t + y3
+
+  let x123 = (x23 - x12) * t + x12
+  let y123 = (y23 - y12) * t + y12
+
+  let x234 = (x34 - x23) * t + x23
+  let y234 = (y34 - y23) * t + y23
+
+  let x1234 = (x234 - x123) * t + x123
+  let y1234 = (y234 - y123) * t + y123
+
+  if(reverse) {
+      return [x1234, y1234, x123, y123, x12, y12, x1, y1]
+  }
+  return [x1, y1, x12, y12, x123, y123, x1234, y1234]
 }
 
 pasition._splitCurves = function (curves, count) {
- 
+  let i = 0,
+      index = 0
+
+  for (; i < count; i++) {
+     let curve =  curves[index]
+     let cs = split(curve[0],curve[1],curve[2],curve[3],curve[4],curve[5],curve[6],curve[7],0.5)
+      curves.splice(index,1)
+      curves.splice(index,0,cs.left,cs.right)
+
+      index+=2
+      if(index >= curves.length-1) {
+          index = 0
+      }
+  }
 }
 
 function sync(shapes, count) {
@@ -104,7 +142,7 @@ pasition.lerp = function (pathA, pathB, t) {
 
 }
 
-pasition.MIM_CURVES_COUNT = 100
+pasition.MIM_CURVES_COUNT = 10
 
 pasition._preprocessing = function(pathA, pathB) {
 
@@ -120,6 +158,29 @@ pasition._preprocessing = function(pathA, pathB) {
   }
 
   clonePathA = sort(clonePathA, clonePathB)
+
+
+  clonePathA.forEach(function (curves, index) {
+
+    let lenA = curves.length,
+        lenB = clonePathB[index].length
+
+    if (lenA > lenB) {
+        if (lenA < pasition.MIM_CURVES_COUNT) {
+            pasition._splitCurves(curves, pasition.MIM_CURVES_COUNT - lenA)
+            pasition._splitCurves(clonePathB[index], pasition.MIM_CURVES_COUNT - lenB)
+        } else {
+            pasition._splitCurves(clonePathB[index], lenA - lenB)
+        }
+    } else if (lenA < lenB) {
+        if (lenB < pasition.MIM_CURVES_COUNT) {
+            pasition._splitCurves(curves, pasition.MIM_CURVES_COUNT - lenA)
+            pasition._splitCurves(clonePathB[index], pasition.MIM_CURVES_COUNT - lenB)
+        } else {
+            pasition._splitCurves(curves, lenB - lenA)
+        }
+    }
+  })
   
   clonePathA.forEach(function(curves, index) {
     clonePathA[index] = sortCurves(curves, clonePathB[index])
@@ -177,7 +238,7 @@ pasition.animate = function (option) {
     let percent = easing(dt / time)
     outShape = pasition._lerp(pathArr[0], pathArr[1], percent)
     progress(outShape, percent)
-    tickId = setTimeout(tick, 500)
+    tickId = requestAnimationFrame(tick)
   }
   tick()
 }
